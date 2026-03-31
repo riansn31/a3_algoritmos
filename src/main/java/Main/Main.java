69929aad73316f72b16029b13c10825aabaec070
Rian Santos <rian.sn31@gmail.com>
2026-03-31 14:09:45 -0300

    fix: menu principal feito

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -4,15 +4,51 @@ import javax.swing.JOptionPane;
 public class Main {
 
     public static void main(String[] args) {
-        String opmenu = JOptionPane.showInputDialog("XYZ COMERCIO DE PRODUTOS LTDA.\n"
-                + "SISTEMA DE CONTROLE DE ESTOQUE  \n"
-                + "MENU PRINCIPAL \n"
-                + "1 - CADASTRO DE PRODUTOS \n"
-                + "2 - MOVIMENTAÇÃO \n"
-                + "3 - REAJUSTE DE PREÇOS \n"
-                + "4 - RELATÓRIOS \n"
-                + "0 - FINALIZAR \n"
-                + "OPÇÃO : ");
-        
+        String menu = """
+                XYZ COMERCIO DE PRODUTOS LTDA.
+                SISTEMA DE CONTROLE DE ESTOQUE
+                
+                1 - CADASTRO DE PRODUTOS
+                2 - MOVIMENTAÇÃO
+                3 - REAJUSTE DE PREÇOS
+                4 - RELATÓRIOS
+                0 - FINALIZAR
+                
+                OPÇÃO :""";
+        String opmenu;
+        do{
+            opmenu = JOptionPane.showInputDialog(null, menu);
+            if (opmenu == null){
+                int finalizar = JOptionPane.showConfirmDialog(null,"DESEJA REALMENTE SAIR?", "SAIR" , JOptionPane.YES_NO_OPTION);
+                if (finalizar == JOptionPane.YES_NO_OPTION)break;
+                else continue;
+            }
+            switch(opmenu){
+                case "1": 
+                    JOptionPane.showMessageDialog(null, "Abrindo Cadastro...");
+                    break;
+                case "2": 
+                    JOptionPane.showMessageDialog(null, "Abrindo Movimentação...");
+                case "3": 
+                    JOptionPane.showMessageDialog(null, "Abrindo Reajuste...");
+                    break;
+                case "4": 
+                    JOptionPane.showMessageDialog(null, "Abrindo Relatórios...");
+                    break;
+                case "0": {
+                    int confirm = JOptionPane.showConfirmDialog(null,"DESEJA REALMENTE FINALIZAR?", "Confirmação" , JOptionPane.YES_NO_OPTION);
+                    if (confirm == JOptionPane.YES_OPTION) {
+                        JOptionPane.showMessageDialog(null, "Sistema encerrado.");
+                        opmenu = "0";
+                    }else{
+                        opmenu = "";
+                    }
+                }
+                default:
+                    JOptionPane.showMessageDialog(null, "OPÇÃO INVÁLIDA!");
+                    break;
+            }
+        }
+        while(!opmenu.equals("0"));
     }
 }
