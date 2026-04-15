ef087b6f03a7f66cee908584e2b0f537ae6df8fc
riansn31 <rian.sn31@gmail.com>
2026-04-15 16:08:08 +0100

    docs: adicionar comentários explicativos no código da Main

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,9 +2,13 @@ package Main;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Programa principal do controle de estoque..
+ */
 public class Main {
 
     public static void main(String[] args) {
+        //String que vai conter o texto do menu
         String menu = """
                 XYZ COMERCIO DE PRODUTOS LTDA.
                 SISTEMA DE CONTROLE DE ESTOQUE
@@ -16,9 +20,12 @@ public class Main {
                 0 - FINALIZAR
                 
                 OPÇÃO :""";
+        //variável que guarda a escolha do usuário
         String opmenu;
+        //loop do menu principal
         do {
             opmenu = JOptionPane.showInputDialog(null, menu);
+            //if para verificar se o usuário realmente deseja sair
             if (opmenu == null) {
                 int finalizar = JOptionPane.showConfirmDialog(null, "DESEJA REALMENTE SAIR?", "SAIR", JOptionPane.YES_NO_OPTION);
                 if (finalizar == JOptionPane.YES_NO_OPTION) {
@@ -27,20 +34,22 @@ public class Main {
                     continue;
                 }
             }
+            //estrutura pra ler a opção que o usuário escolheu e decidir qual método vai executar
             switch (opmenu) {
                 case "1":
                     JOptionPane.showMessageDialog(null, "Abrindo Cadastro...");
                     break;
                 case "2":
                     JOptionPane.showMessageDialog(null, "Abrindo Movimentação...");
+                    break;
                 case "3":
                     JOptionPane.showMessageDialog(null, "Abrindo Reajuste...");
                     break;
                 case "4":
                     JOptionPane.showMessageDialog(null, "Abrindo Relatórios...");
-                    menuRelatorios();
+                    menuRelatorios();//chama o método menuRelatorios()
                     break;
-                case "0": 
+                case "0":
                     int confirm = JOptionPane.showConfirmDialog(null, "DESEJA REALMENTE FINALIZAR?", "Confirmação", JOptionPane.YES_NO_OPTION);
                     if (confirm == JOptionPane.YES_OPTION) {
                         JOptionPane.showMessageDialog(null, "Sistema encerrado.");
@@ -53,10 +62,15 @@ public class Main {
                     JOptionPane.showMessageDialog(null, "OPÇÃO INVÁLIDA!");
                     break;
             }
-        } while (!opmenu.equals("0"));
+        } while (!opmenu.equals("0"));//mantém o loop até o usuário escolher sair
     }
 
+    /**
+     * Menu do cadastro
+     */
     public static void menuCadastro() {
+
+        //String que vai conter o texto do menu
         String menuCad = """
         --- CADASTRO DE PRODUTOS ---
         1 - Incluir Produto
@@ -67,12 +81,16 @@ public class Main {
         
         OPÇÃO:""";
 
+        //variável que guarda a escolha do usuário
         String op;
+        //loop do menu de cadastro
         do {
+            //se o usuário colocar algum valor nulo o menu encerra e volta pro principal
             op = JOptionPane.showInputDialog(null, menuCad);
             if (op == null) {
                 break;
             }
+            //estrutura pra ler a opção que o usuário escolheu e decidir qual método vai executar
             switch (op) {
                 case "1":
                     JOptionPane.showMessageDialog(null, "Incluir Produto");
@@ -89,11 +107,15 @@ public class Main {
                     JOptionPane.showMessageDialog(null, "Opção inválida!");
                     break;
             }
-        } while (!"0".equals(op));
+        } while (!"0".equals(op));//mantém o loop até o usuário escolher sair
 
     }
 
+    /**
+     * Menu de relatórios
+     */
     public static void menuRelatorios() {
+        //String que vai conter o texto do menu
         String menuRel = """
         --- RELATÓRIOS ---
         1 - Relatórios de Inventário
@@ -103,13 +125,17 @@ public class Main {
         0 - Voltar ao Menu Principal
         
         OPÇÃO:""";
-
+        
+        //variável que guarda a escolha do usuário
         String op;
+        //loop do menu de cadastro
         do {
+            //se o usuário colocar algum valor nulo o menu encerra e volta pro principal
             op = JOptionPane.showInputDialog(null, menuRel);
             if (op == null) {
                 break;
             }
+            //estrutura pra ler a opção que o usuário escolheu e decidir qual método vai executar
             switch (op) {
                 case "1":
                     JOptionPane.showMessageDialog(null, "Abrindo Relatórios de Inventário...");
@@ -130,7 +156,7 @@ public class Main {
                     break;
             }
 
-        } while (!"0".equals(op));
+        } while (!"0".equals(op));//mantém o loop até o usuário escolher sair
 
     }
 }
